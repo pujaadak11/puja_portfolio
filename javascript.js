7d59be20f6a9c82867caa62cdc86ef339b9ba5b6
pujaadak11 <129388893+pujaadak11@users.noreply.github.com>
2023-03-30 16:39:47 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>personal portfolio website</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="It's my portfolio, Puja Adak">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<!-- custom cursors  -->
+
+
+<div id="menu-bars" class="fas fa-bars"></div>
+    
+<!-- header section starts  -->
+
+<header>
+
+    <a href="#" class="logo"> <span>PORTFOLIO</span> </a>
+    <nav class="navbar">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#education">Education</a>
+        <a href="#experience">Extra Curricular & Interests</a>
+        <a href="#portfolio">Certification & Training</a>
+        <a href="#contact">Contact</a>
+    </nav>
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <span class="hi"> Hi there... </span>
+        <h3> i am<span> Puja Adak </span> </h3>
+        <p class="info"> Student of Electronics & Communication Engineering </p>
+        <p class="text">  </p>
+        <a href="#about" class="btn">About me</a>
+    </div>
+
+    <div class="image">
+        <img src="images/dp.jpeg" alt="">
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+<h1 class="heading"><i class="fas fa-user-circle"></i>  <span> about </span> me </h1>
+
+<div class="row-1">
+
+    <div class="image">
+        <img src="images/pic.jpeg" alt="">
+    </div>
+
+    <div class="content">
+        <p>I'm a 3rd year student pursuing btech in electronics and communication engineering . I have coding proficiency in languages like c, c++,sql. I also have intermediate knowledge in front-end web development and data structures. MY subjects of interest from ECE are microprocessors and digital electronics. I am a confident and optimistic person , a hardworking fast learner who possesses leadership ,teamwork qualities and excel in collaborative environment.</p>
+        <div class="box-container">
+            <div class="box">
+                <p> <span> DOB : </span>03.11.2000  </p>
+                <p> <span> Phone : </span> 8961414292 </p>
+                <p> <span> E-mail : </span> puja.adak0311@gmail.com </p>
+            </div>
+            <div class="box">
+                <p> <span> Residence : </span> Howrah, india </p>
+                <p> <span> Languages known : </span> English,Bengali,Hindi  </p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<h1 class="heading"><i class="fas fa-laptop-code"></i> <span> my </span> skills </h1>
+
+<div class="row-2">
+
+    <div class="skills">
+        <div class="progress">
+            <h3> c <span> 95% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+        <div class="progress">
+            <h3> c++ <span> 80% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+        <div class="progress">
+            <h3> sql <span> 75% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+        <div class="progress">
+            <h3> html/css <span> 85% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+        <div class="progress">
+            <h3> Data Structures <span> 80% </span> </h3>
+            <div class="bar"> <span></span> </div>
+        </div>
+    </div>
+
+    <div class="box-container">
+
+        <div class="box">
+            <p> Experience</p>
+
+            <h3> 
+                
+                >> FRESHER </h3>
+        </div>
+       
+
+    </div>
+
+
+
+</section>
+
+<!-- about section ends -->
+
+<!-- education section starts  -->
+<section class="head" id="head">
+    <h1 class="heading">
+        <i class="fas fa-graduation-cap"></i>
+        <span> </span> EDUCATION </h1>
+</section>
+
+<section class="education" id="education">
+
+<div class="table">
+    <table>
+        
+        <tr>
+          <th>YEAR</th>
+          <th>DEGREE/EXAM</th>
+          <th>INSTITUTION</th>
+          <th>MARKS(%)</th>
+          
+        </tr>
+        <tr>
+          <td>2022</td>
+          <td>B.TECH (ECE)</td>
+          <td>St. Thomas' College Of Engineering & Technology(affiliated to MAKAUT)</td>
+          <td>9.6(upto 5th sem)</td>
+        </tr>
+        <tr>
+            <td>2018</td>
+            <td>12th boards(WBCHS) </td>
+            <td>Santragachi Kedernath Institution for girls'</td>
+            <td>64%</td>
+          </tr>
+          <tr>
+            <td>2016</td>
+            <td>10th boards(WBBSE)</td>
+            <td>Buxsarah Surendranath Girls' High School</td>
+            <td>61%</td>
+          </tr>
+      </table>
+      </div>
+
+</section>
+
+<!-- education section ends -->
+
+<!-- experience section starts  -->
+
+<section class="experience" id="experience">
+
+    <h1 class="heading"> <i class="fas fa-futbol"></i> <span> EXTRA CURRICULAR </span> & INTERESTS </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="content">
+                <span> DANCER  </span>
+                <h3>Rabindra Nritya</h3>
+                <p>I have been a passionate dancer for 15 years now. Specialization in Indian Classical dance form - Bharatnatyam. Currently a student of 5th year in bharatnatyam. I have also passed 3rd year examination in Rabindra nritya.</p>
+            </div>
+        </div> 
+
+        <div class="box">
+            <div class="content">
+                <span> HOBBIES </span>
+                <ul>
+                    <li>watching movies and webseries</li>
+                    <li><p1>reading story books and novels</p1></li>
+                    <li>Travelling</li>
+                  </ul>
+                
+            </div>
+        </div>
+        <div class="box">
+            <div class="content">
+                <span> OTHER AREAS OF INTERESTS </span>
+                <ul>
+                    <li>Sports(especially Cricket and football)</li>
+                    <li><p1>Photography</p1></li>
+                    <li>Management and entrepreneurship</li>
+                    <li><p1>keeping updates about latest technologies</p1></li>
+                    <li>trekking,scuba diving and paragliding</li>
+                  </ul>
+            </div>
+        </div>
+       
+
+    </div>
+
+</section>
+
+<!-- experience section ends -->
+
+<!-- portfolio section starts  -->
+
+<section class="portfolio" id="portfolio">
+
+    <h1 class="heading"> 
+        <i class="fas fa-medal"></i>
+        <span> certification </span> and training </h1>
+        <div class="clm">
+            <div class="space">
+            <div>
+            <img src="images/udemy.png" alt="udemy" class="image" /></div>
+            </div>
+            <div >
+            <img src="images/datacamp.png" alt="datacamp" class="image">
+            </div>
+        </div>
+            <div class="clm2">
+            <div >
+                <img src="images/coursera.png" alt="coursera" class="image" />
+                </div>
+
+                <div >
+                    <img src="images/code.png" alt="code" class="image"/>
+                    </div>    
+        </div>
+        <div class="clm3">
+            <div class="space">
+            <div>
+            <img src="images/code gladiators.png" alt="udemy" class="image1" /></div>
+            </div>
+            <div >
+            <img src="images/presi.jpg" alt="datacamp" class="image1">
+            </div>
+        </div>
+        </div>
+
+
+        <div class="box-container">
+        <div class="box">
+            <div class="content">
+                <span> TRAININGS </span>
+                <ul>
+                    <li>Python, basics and beyond-<p1> Nullstack Technologies Pvt Ltd.</p1></li>
+                    <li>Industrial Training on Telecom Technology organised by <p1>Netaji Subhas chandra bose telecom training center, BSNL</p1></li>
+                  </ul>
+                
+            </div>
+        </div>
+        </div>
+
+ 
+<!-- portfolio section ends --> 
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+    <h1 class="heading"> contact <span> me </span> </h1>
+
+    <div class="icons-container">
+
+        <div class="icons">
+            <i class="fas fa-envelope"></i>
+            <h3>My E-mail</h3>
+            <p>puja.adak0311@gmail.com</p>
+           </div>
+
+        <div class="icons">
+            <i class="fas fa-phone"></i>
+            <h3>My number</h3>
+            <p>8961414292</p>
+            <p>8617261046</p>
+        </div>
+
+        <div class="icons">
+            <i class="fas fa-map-marker-alt"></i>
+            <h3>My address</h3>
+            <p>Jagacha G.I.P Colony Old Post Office Lane Howrah-711112</p>
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <form action="">
+
+            <input type="text" placeholder="name" class="box">
+            <input type="email" placeholder="email" class="box">
+            <input type="number" placeholder="number" class="box">
+
+            <textarea name="" placeholder="message" id="" cols="30" rows="10"></textarea>
+
+            <input type="submit" class="btn" value="send message">
+
+        </form>
+
+    </div>
+
+
+</section>
+
+<!-- contact section ends -->
+
+<!-- footer section  -->
+<footer class="footer"> Created by <span> Puja Adak </span> | all rights reserved! </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="javascript.js"></script>
+
+</body>
+</html>
